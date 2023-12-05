8528451ddb2aa142b43d6330feb85d05d3cadecd
Yasir Nabil <yasir.nabil5343@gmail.com>
2023-12-06 03:19:38 +0600

    Commit-108: Refactor on middlewares

--- Server/src/middlewares/tokenAuthenticator.js
+++ Server/src/middlewares/tokenAuthenticator.js
@@ -2,20 +2,24 @@ const jwt = require('jsonwebtoken');
 
 // * Middleware to authenticate JWT access token
 const authenticateToken = (req, res, next) => {
-    const authHeader = req?.headers?.authorization;
-    const [type, token] = authHeader?authHeader.split(' '): [];
-    if (!token) {
-        res.status(401).json({ message: 'Unauthorized token' });
-        return;
-    }
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) {
-            res.status(401).json({ message: `Unauthorized token for type ${type}` });
+    try {
+        const authHeader = req?.headers?.authorization;
+        const [type, token] = authHeader?authHeader.split(' '): [];
+        if (!token) {
+            res.status(401).json({ message: 'Unauthorized token' });
             return;
         }
-        req.user = user;
-        next();
-    });
+        jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+            if (err) {
+                res.status(401).json({ message: `Unauthorized token for type ${type}` });
+                return;
+            }
+            req.user = user;
+            next();
+        });
+    } catch (err) {
+        res.status(500).json({ message: 'Something went wrong' });
+    }
 };
 
 module.exports = {
